13df674feb13217deb160f3aa2d24ea6263c12f0
YuliaBakhar <bakhar.yulia@gmail.com>
2018-09-23 22:48:23 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -4,4 +4,27 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  loveTriangles = 0
+  
+  for(i=0; i<preferences.length; i++){
+  ind1 = i+1;
+  elem1 = preferences[ind1-1];
+  
+  if (ind1!=elem1){
+      
+    ind2 = elem1;
+    elem2 = preferences[ind2-1];
+    
+    if (ind2!=elem2 && elem2!=ind1){
+      ind3 = elem2;
+      elem3 = preferences[ind3-1];
+        
+      if (elem3 == ind1){ 
+        loveTriangles = loveTriangles + 1;
+      }
+    }
+  }
+    
+  }
+  return loveTriangles / 3;
 };
