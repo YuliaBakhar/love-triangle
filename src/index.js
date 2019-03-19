478ca220cfe52fb71ad310e830412f01082160c1
Yulia <bakhar.yulia@gamil.com>
2019-03-19 11:22:31 +0300

    refactor: refactoring

--- src/index.js
+++ src/index.js
@@ -2,24 +2,22 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-    loveTriangles = 0
-  
-    for(i=0; i<preferences.length; i++){
-    firstIndex = i+1;
-    firstValue = preferences[firstIndex-1];
-  
-    if (firstIndex!=firstValue){
-      
-      secondIndex = firstValue;
-      secondValue = preferences[secondIndex-1];
-    
-    if (secondIndex!=secondValue && secondValue!=firstIndex){
-        thirdIndex = secondValue;
-        thirdValue = preferences[thirdIndex-1];
-        
-       if (thirdValue == firstIndex){ 
+module.exports = getLoveTrianglesCount = (preferences = []) => {
+  let loveTriangles = 0;
+
+  for (i = 0; i < preferences.length; i++) {
+    const firstIndex = i + 1;
+    const firstValue = preferences[firstIndex - 1];
+
+    if (firstIndex != firstValue) {
+      const secondIndex = firstValue;
+      const secondValue = preferences[secondIndex - 1];
+
+      if (secondIndex != secondValue && secondValue != firstIndex) {
+        const thirdIndex = secondValue;
+        const thirdValue = preferences[thirdIndex - 1];
+
+        if (thirdValue == firstIndex) {
           loveTriangles = loveTriangles + 1;
         }
       }
